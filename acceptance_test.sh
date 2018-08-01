29a7b4563e9d374c4bacc626241ce5905b42c1d1
Adrian Cofie <acofie@sapient.com>
2018-08-01 14:49:41 -0400

    Modify permissions

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 # Check the HTTP status code for the application
-test $(curl -Is localhost:9090 | head -n 1 | awk '{ print $2}') -eq 200
-
+t $(curl -LI localhost:9090 -o /dev/null -w '%{http_code}\n' -s) -eq 200
 
